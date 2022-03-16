66ac6fd8c0bae418594fbd4b92ef8faa2e202ba6
polaGamal <bola.gamal222@gmail.com>
2022-03-16 16:41:37 -0700

    adding currency and new files

--- eShtry/Constant/Constant.swift
+++ eShtry/Constant/Constant.swift
@@ -28,6 +28,8 @@ let orderInfo = "https://f36da23eb91a2fd4cba11b9a30ff124f:shpat_8ae37dbfc644112e
 
 let countries = "https://f36da23eb91a2fd4cba11b9a30ff124f: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@jets-ismailia.myshopify.com/admin/api/2022-01/countries.json"
 
+let currencies = "https://f36da23eb91a2fd4cba11b9a30ff124f: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@jets-ismailia.myshopify.com/admin/api/2022-01/currencies.json"
+
 
 enum typeOfButton{
     case plusBtn
