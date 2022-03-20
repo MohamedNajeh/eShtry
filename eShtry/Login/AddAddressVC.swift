280b0bbb7a1a5a120a0aa4825ea9cd6ae594c1f4
polaGamal <bola.gamal222@gmail.com>
2022-03-20 13:38:15 -0700

    modify assets

--- eShtry/Login/AddAddressVC.swift
+++ eShtry/Login/AddAddressVC.swift
@@ -2,28 +2,42 @@
 //  AddAddressVC.swift
 //  eShtry
 //
-//  Created by Pola on 3/20/22.
+//  Created by Pola on 3/19/22.
 //
 
 import UIKit
 
-class AddAddressVC: UIViewController {
-
+class AddAddressVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var imageNoAddressesOutlet: UIImageView!
+    @IBOutlet weak var titleScreenAddressesOutlet: UILabel!
+    @IBOutlet weak var addresseTableOutlet: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addresseTableOutlet.tableFooterView = UIView()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addNewAddress(_ sender: Any) {
+        let addressVC = storyboard?.instantiateViewController(identifier: "AddressVC") as! AddressVC
+        navigationController?.pushViewController(addressVC, animated: true)
     }
-    */
+    
+ 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return 2}
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "addressCell", for: indexPath)
+//        let title = cell.viewWithTag(1) as? UILabel
+//        let name = cell.viewWithTag(2) as? UILabel
+//        let phone = cell.viewWithTag(3) as? UILabel
+//        let cityCountry = cell.viewWithTag(4) as? UILabel
+//        let address = cell.viewWithTag(5) as? UILabel
 
+        return cell
+       }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {return 200}
 }
