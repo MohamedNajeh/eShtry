84b84ad627a3b37d3f68bc1266b6701851796414
eslamoomohamed <eslamomohamed@yahoo.com>
2022-03-20 23:50:01 +0200

    last addressVC isa

---
+++ eShtry/testAddress.swift
@@ -0,0 +1,43 @@
+//
+//  testAddress.swift
+//  eShtry
+//
+//  Created by eslam mohamed on 20/03/2022.
+//
+
+import UIKit
+
+class testAddress: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var imageNoAddressesOutlet: UIImageView!
+    @IBOutlet weak var titleScreenAddressesOutlet: UILabel!
+    @IBOutlet weak var addresseTableOutlet: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addresseTableOutlet.tableFooterView = UIView()
+    }
+    
+
+    @IBAction func addNewAddress(_ sender: Any) {
+        let addressVC = storyboard?.instantiateViewController(identifier: "AddressVC") as! AddressVC
+        navigationController?.pushViewController(addressVC, animated: true)
+    }
+    
+ 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return 2}
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "addressCell", for: indexPath)
+//        let title = cell.viewWithTag(1) as? UILabel
+//        let name = cell.viewWithTag(2) as? UILabel
+//        let phone = cell.viewWithTag(3) as? UILabel
+//        let cityCountry = cell.viewWithTag(4) as? UILabel
+//        let address = cell.viewWithTag(5) as? UILabel
+
+        return cell
+       }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {return 200}
+}
